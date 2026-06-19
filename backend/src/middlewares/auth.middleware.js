a16ae77e568e19c73719c39e2ecd871ac97f36b6
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-20 01:42:20 +0530

    link-tree setup done

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import config from "../config/config";
+import config from "../config/config.js";
 
 const authMiddleware = (req, res, next) => {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
