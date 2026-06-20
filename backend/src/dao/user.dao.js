b9fe2ab2b6b070fe97c3c0f2fd9b0261e681c224
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-20 18:52:59 +0530

    DAO added

--- backend/src/dao/user.dao.js
+++ backend/src/dao/user.dao.js
@@ -1,1 +1,24 @@
 import User from "../models/user.model";
+
+// Finding user by email
+async function findUserByEmail(email) {
+  return User.findOne({ email: email.toLowerCase().trim() });
+}
+
+// finding user by email with password
+async function findUserByEmailWithPassword(email) {
+  return User.findOne({ email: email.toLowerCase().trim() }).select(
+    "+password",
+  );
+  //The + means: Include a field that is excluded by default.
+}
+
+async function createUser(userData) {
+  return User.create(userData);
+}
+
+export default {
+  findUserByEmail,
+  findUserByEmailWithPassword,
+  createUser,
+};
