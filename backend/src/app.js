dcceff14303ac44d80cda7fdc393569353db85ec
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-22 23:14:12 +0530

    linktree

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,53 @@
 import express from 'express';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
-import apiRoutes from './routes/index.routes.js';
+import indexRoutes from './routes/index.routes.js';
+import ApiError from './utils/ApiError.js';
+import ApiResponse from './utils/ApiResponse.js';
+import setMiddlewares from "./middlewares/index.middleware.js";
+import errorHandler from "./middlewares/error.middleware.js";
 
-
+function createApp() {
 const app = express();
+setMiddlewares(app);
+
+app.use(morgan('dev'));      // Setting up morgan for logging every request
+app.use(express.json());  //Converts JSON body into JavaScript object.
+app.use(cookieParser());  //Parses cookies from incoming requests.
+app.use('/api', indexRoutes);
+
+// Send unknown routes to the centralized error handler.
+app.use((req,res,next) => {
+    next(new ApiError(404, `Route not found: ${req.originalUrl}`))
+})
+
+app.use(errorHandler)  //This middleware catches all errors passed using:next(error)
 
 
-app.use(morgan('dev'));
-app.use(express.json());
-app.use(cookieParser());
-app.use('/api', apiRoutes);
-
-
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+app.get('/health', (req, res) => {
+    return ApiResponse(res,200,"server is healthy")
 });
 
-export default app;
+return app
+
+}
+
+export default createApp;
+
+// Client Request
+//       ↓
+// Morgan Logger
+//       ↓
+// express.json()
+//       ↓
+// cookieParser()
+//       ↓
+// Custom Middlewares
+//       ↓
+// /api Routes
+//       ↓
+// 404 Middleware
+//       ↓
+// Error Handler
+//       ↓
+// Response
