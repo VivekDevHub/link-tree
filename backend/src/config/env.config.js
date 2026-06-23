5525d8d6b5db1b9af010547a64d1c75d3bcfc195
VivekDevHub <vivekkushwahpersonal@gmail.com>
2026-06-23 23:33:22 +0530

    linktree

--- backend/src/config/env.config.js
+++ backend/src/config/env.config.js
@@ -1,9 +1,16 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
 
-config();  //This line reads the .env file and stores all variables inside:
-
+config();
 
 export const PORT = process.env.PORT || 5000;
-export const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/linktree';
+export const MONGO_URI = process.env.MONGO_URI || "";
 export const JWT_SECRET = process.env.JWT_SECRET || "";
-
+export const IMAGEKIT_PUBLIC_KEY = process.env.IMAGEKIT_PUBLIC_KEY || "";
+export const IMAGEKIT_PRIVATE_KEY = process.env.IMAGEKIT_PRIVATE_KEY || "";
+export const IMAGEKIT_URL_ENDPOINT = process.env.IMAGEKIT_URL_ENDPOINT || "";
+export const SMTP_HOST = process.env.SMTP_HOST || "";
+export const SMTP_PORT = process.env.SMTP_PORT || "";
+export const SMTP_USER = process.env.SMTP_USER || "";
+export const SMTP_PASS = process.env.SMTP_PASS || "";
+export const SMTP_FROM = process.env.SMTP_FROM || "Linkters <linkters@bhavyadhanwani.dev>";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
